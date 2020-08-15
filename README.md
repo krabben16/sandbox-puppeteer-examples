fae0c9b55624a33dded35dd61551acf173496746
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-08-15 14:57:20 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # test-puppeteer-examples
-GoogleChromeLabs/puppeteer-examplesの動作確認
+
+```shell
+docker-compose up -d
+docker-compose exec puppeteer-examples bash
+```
+
+```shell
+mkdir /home/chromium
+cd /home/chromium
+bash /home/src/downloadChromium.sh 727972
+ln -s ./727972/chrome-linux ./latest
+
+cd /home/src
+mkdir output
+node lazyimages_without_scroll_events.js -u https://css-tricks.com/examples/LazyLoading/ -o output/result.html
+```
---
+++ src/downloadChromium.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+REVISION=$1
+
+if [ -d $REVISION ] ; then
+  echo "already have this version"
+  exit
+fi
+
+ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
+
+ZIP_FILE="${REVISION}-chrome-linux.zip"
+
+echo "fetching $ZIP_URL"
+
+mkdir $REVISION
+pushd $REVISION
+
+# -#: change progress meter
+curl -# $ZIP_URL > $ZIP_FILE
+
+echo "unzipping.."
+unzip $ZIP_FILE
+
+popd
